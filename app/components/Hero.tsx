85909aba88b1865da2dfdf787ae160a49f3032db
Дмитрий Заболотский <zabolotskiide@gmail.com>
2026-07-05 16:13:40 +0300

    Новый Hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,8 +1,8 @@
 export function Hero() {
   return (
-    <section className="p-20 bg-red-600 text-white text-center">
+    <section className="p-20 bg-red-600 text-white">
       <h1 className="text-6xl font-black">
-        ЭТО НОВЫЙ HERO
+        НОВЫЙ HERO РАБОТАЕТ
       </h1>
     </section>
   );
